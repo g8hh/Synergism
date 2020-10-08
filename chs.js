10cd10bea93f0b90d676a23adcb33d005366856a
by22dgb <5714035+by22dgb@user.noreply.gitee.com>
2020-10-08 17:25:26 +0800

    update chs.js.

--- chs.js
+++ chs.js
@@ -220,9 +220,8 @@ var cnItems = {
     'When bought, rune effectiveness is increased by Log4(Talisman Shards +1) ': '(译者注：注意，与升级等级无关)购买后，符文效果增加 log4(护身符灰底碎片 +1)',
     'When bought, gain Log4(Constant + 1)% more Wow! Cubes and Tesseracts on ascension.': '(译者注：注意，与升级等级无关)购买后，飞升的惊奇方盒和惊奇超立方获取数量增加 log4(数学常数数量 + 1)%',
     'Effect: Mint Production x1e100 (Duh': '效果：铸币厂产量 x1e100 (废话',
-    '': '',
-    '': '',
-    '': '',
+    'Effect: +1 Accelerator Boost.': '效果：增加1个加速器加成。',
+    'Effect: Thank you for getting the server above 30 boosts!': '效果：感谢您让服务器助力超过了30！',
     '': '',
     '': '',
     '': '',
@@ -1406,8 +1405,6 @@ var cnPrefix = {
     'Effect: Cosmic Magnetics will allow you to gain': '效果:宇宙磁学允许你获得',
     'Effect: Your patience will allow you to gain': '效果:你的耐心使你获得',
     'Effect: Grandmasters will produce': '效果:大师会产出',
-    'Effect: All Mythos producers production': '效果:所有神话生产者产量',
-    'Effect: All Crystal producers': '效果:所有水晶生产者产量',
     'Effect: Good day sir! -Willy Wonka': '效果:早上好!-威利·旺克(出自电影 欢乐糖果屋)',
     'Effect: you LOSE! you get NOTHING!': '效果:你输了!你什么也得不到!',
     'Effect: clear as Crystal!': '效果:像水晶一样漂亮!',
@@ -1594,7 +1591,7 @@ var cnRegReplace = new Map([
     [/^Effect: Worker Production(.*)$/, '效果：工人产量$1'], //升级
     [/^Effect: Investment Production(.*)$/, '效果：投资产量$1'], //升级
     [/^Effect: Printer Production(.*)$/, '效果：打印机产量$1'], //升级
-    [/^Effect: Mint Production(.*)(?!Duh)$/, '效果：铸币厂产量$1'], //升级
+    [/^Effect: Mint Production(.*)(?<!Duh)$/, '效果：铸币厂产量$1'], //升级
     [/^Effect: Alchemy Production(.*)$/, '效果：炼金术产量$1'], //升级
     [/^Effect: All Coin production(.*)$/, '效果：所有金币产量$1'], //升级
     [/^Effect: Gain (.+) free multipliers from bought Alchemies.$/, '效果：根据已购买的炼金术数量获得$1个免费加倍器。'], //升级
@@ -1606,11 +1603,11 @@ var cnRegReplace = new Map([
     [/^Effect: Gain (.+)x more Diamonds on prestige$/, '效果：转生的钻石产量增加$1倍'], //升级
     [/^Effect: All coin production is further multiplied by (.+) \[Stacks with upgrade 1\]!$/, '效果：金币产量再增加$1倍[前面为不算上第一个升级原来效果时的数字]！'], //升级
     [/^Effect: \+(.+) Multipliers, \+(.+) Accelerators.$/, '效果：增加$1个加倍器，$2个加速器。'], //升级
-    [/^Effect:$/, '效果：'], //升级
-    [/^Effect:$/, '效果：'], //升级
-    [/^Effect:$/, '效果：'], //升级
-    [/^Effect:$/, '效果：'], //升级
-    [/^Effect:$/, '效果：'], //升级
+    [/^Effect: \+(.+) Accelerators.$/, '效果：增加$1个加速器。'], //升级
+    [/^Effect: \+(.+) Multipliers.$/, '效果：增加$1个加倍器。'], //升级
+    [/^Effect: \+(.+) Accelerator Boosts$/, '效果：增加$1个加速器加成'], //升级
+    [/^Effect: All Crystal producers(.*)$/, '效果：所有水晶生产者产量$1'], //升级
+    [/^Effect: All Mythos producers production(.*)$/, '效果：所有神话生产者产量$1'], //升级
     [/^Effect:$/, '效果：'], //升级
     [/^Effect:$/, '效果：'], //升级
     [/^Effect:$/, '效果：'], //升级

1e954ff6ca7556baaae736a79f7b9da997be35ac
by22dgb <by22dgb@gmail.com>
2021-07-14 22:19:12 +0800

    感谢耗子大佬的帮助

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,3 @@
 export const version = '2.5.5';
 export const testing = false;
-export const lastUpdated = new Date('##LAST_UPDATED##');
+export const lastUpdated = new Date(Date.UTC(2021, 6, 14, 14, 18, 23));

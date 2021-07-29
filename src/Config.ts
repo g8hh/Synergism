144c410340b695cc900c89074aea69cc7f182084
by22dgb <by22dgb@gmail.com>
2021-07-29 21:11:31 +0800

    微调

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,3 @@
 export const version = '2.5.7';
 export const testing = false;
-export const lastUpdated = new Date(Date.UTC(2021, 6, 29, 6, 28, 66));
+export const lastUpdated = new Date(Date.UTC(2021, 6, 29, 6, 28, 6));

8f9eb21b4d9bbbb48bc4b693d0505b4b9d9af90c
by22dgb <by22dgb@gmail.com>
2022-05-05 07:41:24 +0800

    补充

--- chs.js
+++ chs.js
@@ -1680,7 +1680,7 @@ var cnItems = {
     'Your code is either invalid or already used. Try again!': '奖励代码输入错误，或者已经使用过。请再次尝试！',
     'Light Mode': '明亮模式',
     'Dark Mode': '黑暗模式',
-    'You\'re playing v2.9.2 - The Reality Update pt.1 [Last Update: 7:30 UTC 3-May-2022].': '您目前玩的版本为V2.9.2 - 现实升级第一部分 [2022年5月3日 UTC 7:30更新]。',
+    'You\'re playing v2.9.3 - The Reality Update pt.1 [Last Update: 2:7 UTC 5-May-2022].': '您目前玩的版本为V2.9.3 - 现实升级第一部分 [2022年5月5日 UTC 2:7更新]。',
     'Current Bonus: N/A% (offline)!': '当前加成：无(因为离线)！',
     'Current Bonus: N/A% (unfocused)!': '当前加成：无(因为非前台)！',
     'Event Status': '活动状态',
